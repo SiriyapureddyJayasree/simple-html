d6980421af39418cf6d466244722c1a0d4f37e70
SiriyapureddyJayasree <jayasreereddy189@gmail.com>
2026-07-12 11:02:46 +0530

    rename and add new funcfion

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+void hello_world(string name)
+{
+	cout<< "****"<<name<<"****"<<std::endl;
+}
+
+int main()
+{
+	string na = " hello Jayshree"
+	hello_world(na)
+	return 0;
+}
